b7ff46985e744ed94c3ab21eda101ee82c6e3f33
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2022-05-21 12:30:47 +0300

    make axios on other base and get data

--- server.js
+++ server.js
@@ -5,16 +5,23 @@ const axios = require("axios");
 const app = express(); //* инициализируем экземпляр проекта
 
 const PORT = process.env.PORT || 8081;
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon/1";
 
 app.use(express.json()); //* для парса запроса json и запищи в req.body
 app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/pokemon", async (req, res) => {
-  const pokemon = await axios("https://pokeapi.co/api/v2/ability/", {
-    params: { id: 1 },
-  });
+  try {
+    const pokemon = await axios(BASE_URL, {
+      params: {
+        id: 1,
+      },
+    });
 
-  res.json({ pokemon: pokemon.data.name });
+    res.json({ pokemon: pokemon.data.name });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 app.use((req, res) => {
