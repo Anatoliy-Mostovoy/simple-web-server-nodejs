d294f2c5a390a613fa49e74a4c841009acdc6b08
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2022-05-21 12:15:17 +0300

    use axios

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const path = require("path");
+const axios = require("axios");
+
 const app = express(); //* инициализируем экземпляр проекта
 
 const PORT = process.env.PORT || 8081;
 
 app.use(express.json()); //* для парса запроса json и запищи в req.body
 app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("/pokemon", async (req, res) => {
+  const pokemon = await axios("https://pokeapi.co/api/v2/ability/", {
+    params: { id: 1 },
+  });
+
+  res.json({ pokemon: pokemon.data.name });
+});
+
 app.use((req, res) => {
-  res.json({ name: "Tolik" });
+  res.send("<h1>Its response in middleware</h1>");
 });
 
 app.listen(PORT, (err) => {
