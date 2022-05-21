babeaefd72292bf2b2035fddb5f0d353c5d3b5f2
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2022-05-21 14:17:46 +0300

    add morgan

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const axios = require("axios");
 const cors = require("cors");
+const morgan = require("morgan");
 require("dotenv").config();
 
 const app = express(); //* инициализируем экземпляр проекта
@@ -11,6 +12,7 @@ const BASE_URL = "https://pokeapi.co/api/v2/pokemon/1";
 
 app.use(express.json()); //* для парса запроса json и запищи в req.body
 app.use(express.static(path.join(__dirname, "/public")));
+app.use(morgan("tiny"));
 app.use(cors()); //*
 
 app.get("/pokemon", async (req, res) => {
